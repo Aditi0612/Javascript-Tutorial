006fc8c997341454884b8b7e8a3e9401dde7ca44
Aditi <aditisrivastava2609@gmail.com>
2022-05-21 11:28:49 +0530

    fourth commit

--- script.js
+++ script.js
@@ -121,6 +121,16 @@ else
 
 
 //8. Loops in Javascript
+var arr=[1,2,3,4,5];
+console.log(arr);
+for(var i=0;i<arr.length;i++)
+{
+    console.log(arr[i]);
+}
 
+arr.forEach(function(element)
+{
+   console.log(element)
+})
 
 
